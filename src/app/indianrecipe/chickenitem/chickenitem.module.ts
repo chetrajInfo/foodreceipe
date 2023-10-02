ad07d0c49a59485b3b461bc3f48f690a8463fe0a
myinfo <chetraj0101@gmail.com>
2023-10-01 23:36:54 -0500

    added mainpage component inside the chickenitem

--- src/app/indianrecipe/chickenitem/chickenitem.module.ts
+++ src/app/indianrecipe/chickenitem/chickenitem.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ButterchickenComponent } from './butterchicken/butterchicken.component';
 import { ChickenmasalaComponent } from './chickenmasala/chickenmasala.component';
+import { MainpageComponent } from './mainpage/mainpage.component';
 
 
 
 @NgModule({
   declarations: [
     ButterchickenComponent,
-    ChickenmasalaComponent
+    ChickenmasalaComponent,
+    MainpageComponent
   ],
   imports: [
     CommonModule
